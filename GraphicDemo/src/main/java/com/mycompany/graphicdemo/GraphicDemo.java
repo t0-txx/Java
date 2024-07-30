2d1767873d41958a3c70d4874a6919492b48f33e
TXX <cm.wirachai@gmail.com>
2024-07-30 15:08:18 +0700

    Add files via upload

---
+++ GraphicDemo/src/main/java/com/mycompany/graphicdemo/GraphicDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.graphicdemo;
+
+/**
+ *
+ * @author com4936
+ */
+public class GraphicDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
