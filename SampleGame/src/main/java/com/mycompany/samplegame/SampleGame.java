fca099e694e5c10474a50f69c91f29dfa7ffcd54
TXX <cm.wirachai@gmail.com>
2024-08-18 21:42:26 +0700

    Add files via upload

---
+++ SampleGame/src/main/java/com/mycompany/samplegame/SampleGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.samplegame;
+
+/**
+ *
+ * @author com4936
+ */
+public class SampleGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
